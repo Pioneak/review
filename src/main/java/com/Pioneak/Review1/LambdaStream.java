09f8f0b307791eca2239efd9cf0b029ef39a4033
empower <pioneak@163.com>
2026-03-16 10:35:30 +0800

    stream加工流程与具体加工

--- src/main/java/com/Pioneak/Review1/LambdaStream.java
+++ src/main/java/com/Pioneak/Review1/LambdaStream.java
@@ -1,4 +1,41 @@
 package com.Pioneak.Review1;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+import java.util.stream.Collectors;
+
 public class LambdaStream {
+
+
+
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<>();
+
+        Random random = new Random();
+        for(int i = 0;i<10;i++)
+        {
+            list.add(random.nextInt(100) + 1);
+        }
+        List<Integer> collectJudge = list.stream().filter(collector->{
+            return collector % 2 == 1;
+        }).collect(Collectors.toList());
+
+        collectJudge.forEach(System.out::println);
+
+        Collection<String> collectFunction = list.stream().map(collector -> {
+            return "幸运号码" + collector;
+        }).collect(Collectors.toList());
+        collectFunction.forEach(System.out::println);
+
+        list.stream().forEach(num->{
+            System.out.println("流水线来了元素 具体操作：吞了" + num);
+        });
+    }
+
+
+
 }
